1cd03cec344c0a50549e4fb4cd734ac587bc93a0
Vidya Mani <vidsmani@gmail.com>
2017-03-27 02:56:13 -0500

    with details update

--- app/src/main/java/com/example/vidya/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/vidya/contactmanager/MainActivity.java
@@ -41,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         if(file.exists())
             read();
